8e4586bbf750bd13d3a1f4e7d2408019600fee9a
Francisco <fromerojovel@gmail.com>
2018-05-06 14:37:50 -0600

    primera

---
+++ src/java/com/Cliente.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com;
+
+/**
+ *
+ * @author javaDefensa
+ */
+public class Cliente {
+    private int idcliente;
+    private String nombrecliente;
+    private int nit;
+
+    public Cliente(int idcliente, String nombrecliente, int nit) {
+        this.idcliente = idcliente;
+        this.nombrecliente = nombrecliente;
+        this.nit = nit;
+    }
+    
+}
